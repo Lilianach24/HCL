e62f5c4138b08d27fb1e018cad59ac4525a555e6
Lilianach24 <88701768+Lilianach24@users.noreply.github.com>
2025-11-10 16:30:21 +0800

    Reformat llava_generate_label.py code structure

--- llava/llava_generate_label.py
+++ llava/llava_generate_label.py
@@ -6,7 +6,6 @@ from transformers import AutoProcessor, LlavaForConditionalGeneration
 
 
 def llava_labeling(dataset, model_name):
-    # 初始化模型
     model_id = "llava-hf/llava-1.5-7b-hf"
     model = LlavaForConditionalGeneration.from_pretrained(
         model_id,
@@ -16,7 +15,6 @@ def llava_labeling(dataset, model_name):
 
     processor = AutoProcessor.from_pretrained(model_id, use_fast=False)
 
-    # 加载数据，执行打标签任务
     LLaVADatasetLoader(
         root='./datasets',
         dataset=dataset,
@@ -34,6 +32,6 @@ if __name__ == '__main__':
     print('---------------------llava_labeling-------------------------')
     llava_labeling('CIFAR100', 'llava')
     # llava_labeling('caltech-101', 'llava')
-    # llava_labeling('stanford_cars', 'llava')
     # llava_labeling('food-101', 'llava')
     # llava_labeling('tiny-imagenet-200', 'llava')
+
